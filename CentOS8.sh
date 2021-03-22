92a1e8af366eeacca070e034fbabac71643e68c5
Daniel <destruktor@docky.us-central1-a.c.peppy-castle-305110.internal>
2021-03-22 16:04:24 +0000

    Wizard for instalation of Docker for CentOS8

---
+++ CentOS8.sh
@@ -0,0 +1,5 @@
+sudo yum remove docker docker-common docker-selinux docker-engine
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce
+systemctl status docker
